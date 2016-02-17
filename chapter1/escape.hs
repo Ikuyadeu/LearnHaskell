ca6cd18448b5bbc19a369b43a9b425147d3858e6
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-02-17 17:50:38 +0900

    finish chapter1

--- chapter1/escape.hs
+++ chapter1/escape.hs
@@ -1,14 +1,10 @@
-escape :: String -> String
-escape str = str >>= escapeAmp >>= escapeOther where
-	escapeAmp '&' = "&amp"
-	escapeAmp c = [c]
-	escapeOther '<' = "&lt"
-	escapeOther '>' = "&gt;"
-	escapeOther '"' = "&quot;"
-	escapeOther c = [c]
+-- $ echo '"&<>' | runghc escape.hs
+-- &quot;&amp&lt&gt;
+
+import HTMLEscapedString
 
 main :: IO ()
 main = do
 	rawString <- getLine
 	let escapedString = escape rawString
-	putStrLn escapedString
+	putHTMLEscapedStrLn escapedString
