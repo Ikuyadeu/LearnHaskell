aaab2d450c349dbce11194389ccb392f187c3088
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-02-21 21:46:52 +0900

    Texas ranges

--- chapter2-starting_out/baby.hs
+++ chapter2-starting_out/baby.hs
@@ -4,3 +4,5 @@ doubleSmallNumber x = if x > 100
 												then x
 												else x * 2
 doubleSmallNumber' x = (if x > 100 then x	else x * 2) + 1
+conanO'Brien = "It's a-me, Conan O'Brien"
+boomBang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
