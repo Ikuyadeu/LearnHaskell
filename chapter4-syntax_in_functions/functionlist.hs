2c96c72015f04c6f3f6c03a7e0e11d05a7a4d7ae
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-02-24 19:09:00 +0900

    let and where

--- chapter4-syntax_in_functions/functionlist.hs
+++ chapter4-syntax_in_functions/functionlist.hs
@@ -15,4 +15,25 @@ first (x, _, _) = x
 
 capital :: String -> String
 capital "" = "Empty string, whoops!"
-capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]  
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+		| a > b = a
+		| otherwise = b
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+		| bmi <= skinny = "You're underweight, you emo, you!"
+		| bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+		| bmi <= fat    = "You're fat! Lose some weight, fatty!"
+		| otherwise     = "You're a whale, congratulations!"
+		where
+			bmi = weight / height ^ 2
+			(skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^2
+    in  sideArea + 2 * topArea
