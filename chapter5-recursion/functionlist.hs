a85fc1e9b06df06ffac4e3e631dfc04fa2dbfeb2
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-02-26 20:08:09 +0900

    add elem and zip

--- chapter5-recursion/functionlist.hs
+++ chapter5-recursion/functionlist.hs
@@ -1,4 +1,54 @@
+-- ghci> maximum [1,2,3,4,5]
+-- 5
 maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "maximum of empty list"
 maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs)  
+maximum' (x:xs) = max x (maximum' xs)
+
+-- ghci> minimum [1,2,3,4,5]
+-- 1
+minimum' :: (Ord a) => [a] -> a
+minimum' [] = error "minimum of empty list"
+minimum' [x] = x
+mminimum' (x:xs) = min x (maximum' xs)
+
+-- ghci> replicate 5 3
+-- [3,3,3,3,3]
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i,Ord i) => i-> [a] -> [a]
+take' n _
+		| n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+-- take x(repeat y) = replicate x y
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+-- ghci> zip' [1,2,3] ['a', 'b']
+-- [(1,'a'),(2,'b')]
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y):zip' xs ys
+
+-- ghci> elem' 5 [1,2,3,4,5]
+-- True
+-- ghci> elem' 6 [1,2,3,4,5]
+-- False
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+		| a == x = True
+		| otherwise = a `elem` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+		let smallerSorted = quicksort [a | a <- xs, a <= x]
+				biggerSorted = quicksort [a | a <- xs, a > x]
+		in	smallerSorted ++ [x] ++ biggerSorted
