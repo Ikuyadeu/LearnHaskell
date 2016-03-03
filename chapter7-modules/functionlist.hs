b2d15bad2634753f9ada12c77e19e992d5b63d4d
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-03-03 14:29:11 +0900

    Data list

--- chapter7-modules/functionlist.hs
+++ chapter7-modules/functionlist.hs
@@ -1,4 +1,13 @@
 import Data.List
+import qualified Data.Map as M
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack =
+	let nlen = length needle
+	in foldl (\acc x -> ((take nlen x == needle) || acc)) False (tails haystack)
+
+on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+f `on` g = \x y -> f (g x) (g y)
