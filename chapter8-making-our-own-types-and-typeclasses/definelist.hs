2b4105b246e55799860ec57a2f7828d446700cd3
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-03-13 20:15:58 +0900

    make Person

--- chapter8-making-our-own-types-and-typeclasses/definelist.hs
+++ chapter8-making-our-own-types-and-typeclasses/definelist.hs
@@ -1,3 +1,12 @@
+module Shapes
+( Point(..)
+, Shape(..)
+, surface
+, nudge
+, baseCircle
+, baseRect
+) where
+
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
@@ -9,17 +18,40 @@ surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) = abs (x2 - x1) * abs (y2 -y1)
 
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
 baseCircle :: Float -> Shape
 baseCircle = Circle (Point 0 0)
 
 baseRect :: Float -> Float -> Shape
 baseRect width height = Rectangle (Point 0 0) (Point width height)
 
-module Shapes
-( Point(..)
-, Shape(..)
-, surface
-, nudge
-, baseCircle
-, baseRect
-) where
+-- data Person = Person String String Int Float String String deriving (Show)
+--
+-- firstName :: Person -> String
+-- firstName (Person firstName _ _ _ _ _) = firstName
+--
+-- lastName :: Person -> String
+-- lastName (Person _ lastName _ _ _ _) = lastName
+--
+-- age :: Person -> Int
+-- age (Person _ _ age _ _ _) = age
+--
+-- height :: Person -> Float
+-- height (Person _ _ _ height _ _) = height
+--
+-- phoneNumber :: Person -> String
+-- phoneNumber (Person _ _ _ _ number _) = number
+--
+-- flavor :: Person -> String
+-- flavor (Person _ _ _ _ _ flavor) = flavor
+
+data Person = Person { firstName :: String
+, lastName :: String
+, age :: Int
+, height :: Float
+, phoneNumber :: String
+, flavor :: String
+} deriving (Show)
