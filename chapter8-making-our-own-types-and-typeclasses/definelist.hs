18c3c63720857f3da3d3f32315f52a8c443d9e93
Ikuyadeu <ikuyadeu@yahoo.co.jp>
2016-04-01 14:33:38 +0900

    Recursive data structures

--- chapter8-making-our-own-types-and-typeclasses/definelist.hs
+++ chapter8-making-our-own-types-and-typeclasses/definelist.hs
@@ -131,4 +131,33 @@ lockers =
   ,(109,(Taken,"893JJ"))
   ,(110,(Taken,"99292"))]
 
-data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+infixr 5 :-:
+data List a = Empty | a  :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5  .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+-- ghci> let nums = [8,6,4,1,7,3,5]
+-- ghci> let numsTree = foldr treeInsert EmptyTree nums
+-- ghci> numsTree
+-- Node 5 (Node 3 (Node 1 EmptyTree EmptyTree) (Node 4 EmptyTree EmptyTree)) (Node 7 (Node 6 EmptyTree EmptyTree) (Node 8 EmptyTree EmptyTree))
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a = treeElem x left
+  | x > a = treeElem x right
